c9c932e9ad9ebab97e02860a8b8033bc3eea8eaf
Diego Rocha <dlmrocha87@gmail.com>
2022-06-10 12:35:01 -0300

    move to contenteditable
    We are using the common strategy of having a mirror above the textarea to handle the highlight system and so on! Now I decided to move the code in the direction of a div with a content editable!

--- Components/MentionTextArea.razor.cs
+++ Components/MentionTextArea.razor.cs
@@ -20,14 +20,6 @@ public partial class MentionTextArea<T> : ComponentBase, IDisposable
 
     private string? _text;
 
-    private class Word
-    {
-        public string Text { get; set; } = null!;
-        public int Caret;
-    }
-
-    private List<Word>? _texts;
-
     [Parameter]
     public string? Text
     {
@@ -36,7 +28,6 @@ public partial class MentionTextArea<T> : ComponentBase, IDisposable
         {
             _text = value;
             TextChanged.InvokeAsync(_text).AndForget();
-            UpdateTexts(_text);
         }
     }
     [Parameter] public EventCallback<string> TextChanged { get; set; }
@@ -48,24 +39,10 @@ public partial class MentionTextArea<T> : ComponentBase, IDisposable
 
     [Parameter] public RenderFragment<T>? SuggestionContentItem { get; set; }
 
-    private ElementReference? _textarea;
+    private ElementReference? _editor;
     private bool _showMentionBox = false;
-    private int? _mentionBoxWidth;
 
-    private int _caretPosition = 0;
-    private int _currentWordIndex = 0;
-
-    private string CurrentWord
-    {
-        get
-        {
-            if (_texts is not null && _currentWordIndex < _texts.Count)
-            {
-                return _texts[_currentWordIndex].Text;
-            }
-            return "";
-        }
-    }
+    private string? CurrentWord { get; set; }
 
     private IEnumerable<T>? _suggestions;
     private object SelectedSuggestionIndex { get; set; } = 0;
@@ -74,64 +51,8 @@ public partial class MentionTextArea<T> : ComponentBase, IDisposable
     {
         if (firstRender)
         {
-            await JS.InvokeVoidAsync("initializeMentionBox", _textarea);
-            _mentionBoxWidth = await JS.InvokeAsync<int>("getElementWidthById", "_mentionBoxContainer");
-        }
-    }
-
-    protected override void OnParametersSet()
-    {
-        _texts = new();
-        _currentWordIndex = 0;
-    }
-
-    private void UpdateTexts(string? text)
-    {
-        if (text is not null)
-        {
-            var caret = 0;
-            _texts = new();
-
-            // FIXME: for some reason if I have an empty space followed by a new line
-            // I'll have an additional string with length 0. It should not happen, I think
-            // the problem is with the regex I'm using to split the text.
-            string[] parts = Regex.Split(text, @"([.,;\s])");
-            foreach (var part in parts)
-            {
-                if (part.Length > 0)
-                {
-                    _texts.Add(new() { Text = part, Caret = caret });
-                }
-                caret += part.Length;
-            }
-        }
-    }
-
-    private int CaretPositionToWordIndex(int caret)
-    {
-        int index = 0;
-        if (_texts is not null)
-        {
-            int acc = 0;
-            foreach (var (txt, i) in _texts.Select((txt, i) => (txt, i)))
-            {
-                index = i;
-                acc += txt.Text.Length;
-                if (acc >= caret)
-                {
-                    break;
-                }
-            }
-        }
-        return index;
-    }
-
-    private async Task UpdateCaretPosition()
-    {
-        if (_textarea is not null)
-        {
-            _caretPosition = await JS.InvokeAsync<int>("getElementCaretPosition", _textarea);
-            _currentWordIndex = CaretPositionToWordIndex(_caretPosition);
+            var reference = DotNetObjectReference.Create(this);
+            await JS.InvokeVoidAsync("mentionEditor.initialize", reference);
         }
     }
 
@@ -147,7 +68,7 @@ public partial class MentionTextArea<T> : ComponentBase, IDisposable
 
     private void DisposeTimer()
     {
-        if (_timer != null)
+        if (_timer is not null)
         {
             _timer.Enabled = false;
             _timer.Elapsed -= OnSearchAsync;
@@ -169,34 +90,8 @@ public partial class MentionTextArea<T> : ComponentBase, IDisposable
 
     public async Task OnItemSelected(T item)
     {
-        if (_texts is not null)
-        {
-            var username = "@" + item.Username!;
-            var word = _texts[_currentWordIndex];
-
-            _texts[_currentWordIndex] = new() { Text = username, Caret = word.Caret };
-
-            var completionLength = username.Length - word.Text.Length;
-            var cursor = word.Caret + username.Length;
-
-            var text = "";
-            foreach (var w in _texts)
-            {
-                text += w.Text;
-            }
-
-            await JS.InvokeVoidAsync("updateMentionBoxText", _textarea, text, cursor);
-            ResetMentions();
-
-            // XXX: as we are adding text from the suggestion we should recalculate all words caret index
-            foreach (var w in _texts.Skip(_currentWordIndex + 1))
-            {
-                w.Caret += completionLength;
-            }
-
-            // update caret position after the suggestion been added
-            _caretPosition = cursor;
-        }
+        await JS.InvokeVoidAsync("mentionEditor.insertMentionAtHighlighted", _editor, item.Username!);
+        ResetMentions();
     }
 
     private void ResetMentions()
@@ -207,80 +102,141 @@ public partial class MentionTextArea<T> : ComponentBase, IDisposable
             _showMentionBox = false;
             _suggestions = null;
             SelectedSuggestionIndex = 0;
+            CurrentWord = null;
         }
     }
 
-    private async Task OpenMentionBox()
+    private void OpenMentionBox()
     {
-        _mentionBoxWidth = await JS.InvokeAsync<int>("getElementWidthById", "_mentionBoxContainer");
-        _showMentionBox = true;
-        _suggestions = null;
-    }
-
-    private async Task ShouldOpenMentionBox()
-    {
-        if (!string.IsNullOrEmpty(CurrentWord) && CurrentWord[0] == '@' && !_showMentionBox)
+        if (!_showMentionBox)
         {
-            await OpenMentionBox();
-            return;
+            _showMentionBox = true;
+            _suggestions = null;
         }
-        ResetMentions();
     }
 
     private async Task CheckKey(KeyboardEventArgs ev)
     {
-        try
+        if (_showMentionBox)
         {
-            if (_textarea is not null)
+            // handle keys if mention box is opened
+            switch (ev.Key)
             {
-                if (_showMentionBox)
-                {
-                    // handle keys if mention box is opened
-                    switch (ev.Key)
+                case "ArrowUp":
+                    // handle next suggestion
+                    SelectedSuggestionIndex = (int)SelectedSuggestionIndex - 1;
+                    if ((int)SelectedSuggestionIndex < 0)
                     {
-                        case "ArrowUp":
-                            // handle next suggestion
-                            SelectedSuggestionIndex = (int)SelectedSuggestionIndex - 1;
-                            if ((int)SelectedSuggestionIndex < 0)
-                            {
-                                SelectedSuggestionIndex = _suggestions!.Count() - 1;
-                            }
-                            return;
-
-                        case "ArrowDown":
-                            // handle next suggestion
-                            SelectedSuggestionIndex = (int)SelectedSuggestionIndex + 1;
-                            if ((int)SelectedSuggestionIndex >= _suggestions!.Count())
-                            {
-                                SelectedSuggestionIndex = 0;
-                            }
-                            return;
-
-                        case "Enter":
-                            await OnItemSelected(_suggestions!.ElementAt((int)SelectedSuggestionIndex));
-                            return;
-
-                        case "Escape":
-                            ResetMentions();
-                            return;
+                        SelectedSuggestionIndex = _suggestions!.Count() - 1;
                     }
-                }
+                    return;
 
-                await UpdateCaretPosition();
-                DisposeTimer();
+                case "ArrowDown":
+                    // handle next suggestion
+                    SelectedSuggestionIndex = (int)SelectedSuggestionIndex + 1;
+                    if ((int)SelectedSuggestionIndex >= _suggestions!.Count())
+                    {
+                        SelectedSuggestionIndex = 0;
+                    }
+                    return;
 
-                await ShouldOpenMentionBox();
-                if (_showMentionBox)
+                case "Enter":
+                    await OnItemSelected(_suggestions!.ElementAt((int)SelectedSuggestionIndex));
+                    return;
+
+                case "Escape":
+                    ResetMentions();
+                    return;
+            }
+        }
+    }
+
+    public class Token
+    {
+        public TokenType Type { get; set; } = TokenType.Text;
+        public string Value { get; set; } = null!;
+    }
+
+    public enum TokenType
+    {
+        Mention,
+        Text,
+    }
+
+    private static List<Token> ParseLine(string? text)
+    {
+        List<Token> tokens = new();
+        if (text is not null)
+        {
+            // FIXME: for some reason if I have an empty space followed by a new line
+            // I'll have an additional string with length 0. It should not happen, I think
+            // the problem is with the regex I'm using to split the text.
+            string[] parts = Regex.Split(text, @"([.,;\s])");
+            foreach (var part in parts)
+            {
+                if (part.Length > 0)
                 {
-                    // only if the mention box is opened we reset/start the timer
-                    StartTimer();
+                    tokens.Add(new()
+                    {
+                        Type = part switch
+                        {
+                            var x when x.StartsWith("@") => TokenType.Mention,
+                            _ => TokenType.Text,
+                        },
+                        Value = part
+                    });
                 }
             }
         }
-        catch (JSException e)
+        return tokens;
+    }
+
+    public class ElementTextContent
+    {
+        public string? Content { get; set; }
+        public int Row { get; set; }
+        public int Col { get; set; }
+    }
+
+    [JSInvokable]
+    public async Task UpdateEditorContent(ElementTextContent content)
+    {
+        if (_editor is not null && content.Content is not null)
         {
-            await JS.InvokeVoidAsync("alert", e.Message);
+            var tokens = ParseLine(content.Content);
+            await JS.InvokeVoidAsync("mentionEditor.updateEditor", tokens, content.Row, content.Col);
         }
+    }
+
+    public class MentionPopover
+    {
+        public string? Username { get; set; }
+        public double Top { get; set; }
+        public double Left { get; set; }
+    }
+
+    // TODO: implement the popover on mouse hover
+    [JSInvokable]
+    public async Task PopoverMentionInfo(MentionPopover p)
+    {
+        // Console.WriteLine($"username: {p.Username}, top: {p.Top}, left: {p.Left}");
+    }
+
+    [JSInvokable]
+    public async Task OnMention(string word)
+    {
+        CurrentWord = word;
+        var isMentionBoxOpened = _showMentionBox;
+        OpenMentionBox();
+        StartTimer();
+        await InvokeAsync(StateHasChanged);
+    }
+
+    [JSInvokable]
+    public async Task OnCloseMentionPopover()
+    {
+        ResetMentions();
+        await InvokeAsync(StateHasChanged);
     }
 
     public void Dispose()
--- wwwroot/mention.js
+++ wwwroot/mention.js
@@ -1,77 +1,396 @@
-const mentionPopoverElementName = "_mentionBoxPopover";
-const mentionBoxContainerName = "_mentionBoxContainer";
+// this enum is defined in MentionTextare.razor.cs
+const TokenType = {
+  Mention: 0,
+  Text: 1,
+};
 
-let mentionContainer = undefined;
+class Editor {
+  dotnetReference = null;
+  content = null;
+  popoverContent = null;
+  editorCurrentLine = null;
+  highlightedMention = null;
 
-function updateFakeTextareaRect(element) {
-  const rect = element.getBoundingClientRect();
+  // save the last highlight avoid us to traverse the entire editor nodes
+  // checking and removing the `highlight` class
+  lastHighlightedWord = null;
+  lastHighlightedLine = null;
 
-  const fakeTextarea = document.getElementsByClassName("fake-textarea")[0];
-  const fakeContainer = document.getElementById("fake-container");
+  currentCaretLocation = {row: 1, col: 1, isCollapsed: false};
 
-  if (fakeTextarea && fakeContainer) {
-    const rectContainer = fakeContainer.getBoundingClientRect();
-    fakeTextarea.style.top = `${rect.top - rectContainer.top}px`;
-    fakeTextarea.style.bottom = `${rectContainer.bottom - rect.bottom}px`;
-    fakeTextarea.style.left = `${rect.left - rectContainer.left}px`;
-    fakeTextarea.style.right = `${rectContainer.right - rect.right}px`;
-  }
-}
-
-function initializeMentionBox(element) {
-  mentionContainer = document.getElementById(mentionBoxContainerName);
-  if (mentionContainer) {
-    // resize the mention box when the screen (mention box container) is resized
-    new ResizeObserver(updateMentionBoxPopoverWidth).observe(mentionContainer);
-  }
-
-  // set the element style
-  if (element) {
-    updateFakeTextareaRect(element);
+  constructor() {
+    if (typeof window.getSelection === "undefined") {
+      // we depend on window.getSelection existing
+      throw new Error(
+        "your browser do not support window.getSelection! The editor will not work properly"
+      );
+    }
   }
 
-  // XXX: we need to prevent default to some keys that should be handled when
-  // the mention box is opened
-  element?.addEventListener("keydown", (ev) => {
-    // check if the mention popover is open
-    let mp = document.getElementsByClassName("_mentionBoxPopover");
-    if (mp.length > 0) {
-      switch (ev.key) {
-        case "ArrowUp":
-        case "ArrowDown":
-        case "Enter":
-        case "Escape":
-          ev.preventDefault();
-          break;
+  initialize(reference) {
+    this.dotnetReference = reference;
+
+    this.content = document.getElementsByClassName("editor")[0];
+
+    this.initializeMentionPopover();
+
+    // XXX: if we don't filter this event we'll send at least two input events with the same data
+    // this is breaking the flow and the layout of the editor
+    this.content.addEventListener("compositionend", async (ev) => {
+      await this.emitEditorUpdate(ev);
+    });
+
+    this.content.addEventListener("input", async (ev) => {
+      if (!ev.isComposing) {
+        await this.emitEditorUpdate(ev);
+      }
+    });
+
+    this.content.addEventListener("keyup", (ev) => {
+      // if (ev.key.length > 1) {
+      // as we highlight the word right after the input event, we should not trigger it here again.
+      // so to avoid it (not 100%) but effectively we dispatch `highlightWordUnderCaret` only when
+      // the key was supposed to not generate an input event
+      this.updateInterface();
+      // }
+    });
+
+    this.content.addEventListener("keydown", (ev) => {
+      // these keybings will be handled in C#
+      if (this.isMentionPopoverOpen) {
+        switch (ev.key) {
+          case "ArrowUp":
+          case "ArrowDown":
+          case "Enter":
+          case "Escape":
+            ev.preventDefault();
+            break;
+        }
+      }
+    });
+
+    this.content.addEventListener("click", (ev) => {
+      this.updateInterface();
+    });
+  }
+
+  updateInterface() {
+    this.currentCaretLocation = this.getEditorCaretLocation();
+
+    this.highlightCurrentLine();
+    this.highlightWordUnderCaret();
+  }
+
+  anchorNodeToElement(node) {
+    if (node instanceof Element) {
+      return node;
+    }
+    return node && this.anchorNodeToElement(node.parentNode);
+  }
+
+  anchorNodeToLine(node) {
+    let line = this.anchorNodeToElement(node);
+    if (line.hasAttribute("data-line")) {
+      return line;
+    }
+    if (line.hasAttribute("data-editor")) {
+      // FIXME: if we backspace in an empty line this state is reachable
+      // this line do not exist in editor
+      throw new Error("line do not exists in editor");
+    }
+    return this.anchorNodeToLine(line.parentElement);
+  }
+
+  appendNewLineEditor() {
+    const line = document.createElement("div");
+    const br = document.createElement("br");
+
+    line.appendChild(br);
+    line.setAttribute("data-line", true);
+    line.classList.add("line");
+
+    this.content.appendChild(line);
+
+    return line;
+  }
+
+  getLineAt(row) {
+    //filter only valid lines
+    const lines = [...this.content.children].filter((n) =>
+      n.hasAttribute("data-line")
+    );
+    if (lines.length === 0) {
+      // the editor has no lines, so we should create one empty line and return it
+      return this.appendNewLineEditor();
+    }
+    return lines[row - 1];
+  }
+
+  getWordIndexAndOffset(line, offset) {
+    let index = 0;
+    // XXX: we are indexing both line and col starting with 1 but here we must index it based on 0
+    offset -= 1;
+    for (let word of line.children) {
+      const len = word.innerText.length;
+      if (len >= offset) {
+        break;
+      }
+      index += 1;
+      offset -= len;
+    }
+
+    return { index, offset };
+  }
+
+  getWordInLine(line, col) {
+    const { index } = this.getWordIndexAndOffset(line, col);
+    return [...line.children][index];
+  }
+
+  getWordAt(row, col) {
+    const line = this.getLineAt(row);
+    return this.getWordInLine(line, col);
+  }
+
+  getEditorCaretLocation() {
+    const selection = window.getSelection();
+    let line = selection.anchorNode;
+
+    let row = 1;
+    let col = 1;
+
+    if (line !== this.content) {
+      line = this.anchorNodeToLine(line);
+      row = Array.prototype.indexOf.call(this.content.children, line) + 1;
+
+      const range = selection.getRangeAt(0);
+      const clone = range.cloneRange();
+
+      // basically select the entire line
+      clone.selectNodeContents(line);
+      // then reduce the selection to the mark at end
+      clone.setEnd(range.endContainer, range.endOffset);
+      // now counting the length of the selected string will result in the caret position
+      // we increment it by 1 because we are indexing lines and columns by one
+      col = clone.toString().length + 1;
+    }
+
+    return { row, col, isCollapsed: selection.isCollapsed };
+  }
+
+  setEditorCaretLocation(row, col) {
+    const range = new Range();
+    const selection = window.getSelection();
+
+    const line = this.getLineAt(row);
+    const { offset } = this.getWordIndexAndOffset(line, col);
+    const word = this.getWordAt(row, col);
+
+    range.setStart(word.firstChild, offset);
+    range.collapse(true);
+
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
+
+  async emitEditorUpdate() {
+    this.currentCaretLocation = this.getEditorCaretLocation();
+
+    const { row, col, isCollapsed } = this.currentCaretLocation;
+
+    // do not try to update editor if caret is a selection
+    if (isCollapsed) {
+      const line = this.getLineAt(row);
+      if (line) {
+        const content = line.innerText;
+        if (content?.length > 0 && content !== "\n") {
+          await this.dotnetReference.invokeMethodAsync("UpdateEditorContent", {
+            content,
+            row,
+            col,
+          });
+        }
       }
     }
-  });
+  }
 
-  // XXX: our textarea should grow the height based on the content, so we can avoid
-  // scrollbar which will make our life a lot harder to keep the fake-textarea synced
-  element?.addEventListener("keyup", (ev) => {
-    element.style.height = Math.max(element.scrollHeight, element.clientHeight) + "px";
-    updateFakeTextareaRect(element);
-  });
-}
+  clearEditor() {
+    while (this.content.firstChild) {
+      this.content.removeChild(this.content.firstChild);
+    }
+  }
 
-function updateMentionBoxPopoverWidth() {
-  const popover = document.getElementsByClassName(mentionPopoverElementName)[0];
-  if (popover && mentionContainer) {
-    popover.style.width = `${mentionContainer.offsetWidth}px`;
+  get isMentionPopoverOpen() {
+    return this.popoverContent.classList.contains("mud-popover-open");
+  }
+
+  insertMentionAtHighlighted(username) {
+    if (this.highlightedMention) {
+      this.highlightedMention.innerText = "@" + username;
+
+      const selection = window.getSelection();
+
+      const range = new Range();
+      range.setEndAfter(this.highlightedMention);
+      range.collapse();
+
+      selection.removeAllRanges();
+      selection.addRange(range);
+
+      this.emitEditorUpdate();
+    }
+  }
+
+  highlightCurrentLine() {
+    // const { row } = this.getEditorCaretLocation();
+    // const line = this.getLineAt(row);
+    // if (line !== this.lastHighlightedLine) {
+    //   if (this.lastHighlightedLine) {
+    //     this.lastHighlightedLine.style.backgroundColor = "white";
+    //   }
+    //   line.style.backgroundColor = "red";
+    //   this.lastHighlightedLine = line;
+    // }
+  }
+
+  highlightWordUnderCaret() {
+    const { row, col, isCollapsed } = this.currentCaretLocation;
+
+    // do not try to highlight word is selection is not collapsed
+    if (isCollapsed) {
+      if (this.lastHighlightedWord) {
+        this.lastHighlightedWord.classList.remove("highlight");
+      }
+
+      const word = this.getWordAt(row, col);
+      if (word.hasAttribute("data-mention")) {
+        this.dotnetReference.invokeMethodAsync("OnMention", word.innerText);
+        this.highlightedMention = word;
+        return;
+      }
+
+      if (this.isMentionPopoverOpen) {
+        this.dotnetReference.invokeMethodAsync("OnCloseMentionPopover");
+      }
+
+      if (
+        word?.hasAttribute("data-word") &&
+        !word.classList.contains("highlight")
+      ) {
+        this.lastHighlightedWord = word;
+        word.classList.add("highlight");
+      }
+    }
+  }
+
+  updateEditor(tokens, row, col) {
+    let line = document.createElement("div");
+    line.setAttribute("data-line", true);
+
+    for (let token of tokens) {
+      const el = document.createElement("span");
+      el.innerText = token.value;
+
+      switch (token.type) {
+        case TokenType.Mention: {
+          el.setAttribute("data-mention", true);
+          el.classList.add("mention");
+
+          el.addEventListener("mouseenter", (ev) => {
+            const rect = el.getBoundingClientRect();
+            this.dotnetReference.invokeMethodAsync("PopoverMentionInfo", {
+              username: token.value,
+              top: rect.top,
+              left: rect.left,
+            });
+          });
+          break;
+        }
+
+        default:
+          el.setAttribute("data-word", true);
+          break;
+      }
+      line.appendChild(el);
+    }
+
+    let oldLine = this.getLineAt(row);
+    this.content.replaceChild(line, oldLine);
+    this.setEditorCaretLocation(row, col);
+    this.highlightWordUnderCaret();
+    // highlightCurrentLine(editor);
+  }
+
+  initializeMentionPopover() {
+    // get the popover content, so we can positionate if according to the mention
+    let popoverElement = document.getElementById(
+      "editor-popover-container"
+    )?.firstElementChild;
+
+    if (!popoverElement) {
+      throw new Error("popoverElement is not in the DOM");
+    }
+
+    // strip the first 8 "popover-" string because the popover content use the same guid defined after it
+    let popoverId = popoverElement.id.substring(8);
+    this.popoverContent = document.getElementById(
+      `popovercontent-${popoverId}`
+    );
+
+    // FIXME: **remove it** hack to fix popover position
+    const originalPlacePopover = window.mudpopoverHelper.placePopover;
+    window.mudpopoverHelper.placePopover = function (
+      popoverNode,
+      classSelector
+    ) {
+      if (popoverNode) {
+        const id = popoverNode.id.substring(8);
+        if (id === popoverId) {
+          window.mentionEditor.placePopover();
+          return;
+        }
+        originalPlacePopover(popoverNode, classSelector);
+      }
+    };
+  }
+
+  placePopover() {
+    if (this.isMentionPopoverOpen) {
+      const rect = this.highlightedMention.getBoundingClientRect();
+      const selfRect = this.popoverContent.getBoundingClientRect();
+
+      // use the native mudPopover to calculate the position
+      const { top, left, offsetX, offsetY } =
+        window.mudpopoverHelper.calculatePopoverPosition(
+          Array.from(this.popoverContent.classList),
+          rect,
+          selfRect
+        );
+
+      this.popoverContent.style.left = left + offsetX + "px";
+      this.popoverContent.style.top = top + offsetY + "px";
+    }
+  }
+
+  getCaretCoordinates() {
+    let x = 0,
+      y = 0;
+    const isSupported = typeof window.getSelection !== "undefined";
+    if (isSupported) {
+      const selection = window.getSelection();
+      if (selection.rangeCount !== 0) {
+        const range = selection.getRangeAt(0).cloneRange();
+        // Collapse the range to the start, so there are not multiple chars selected
+        range.collapse(true);
+        const rect = range.getClientRects()[0];
+        if (rect) {
+          x = rect.left; // since the caret is only 1px wide, left == right
+          y = rect.top; // top edge of the caret
+        }
+      }
+    }
+    return { x, y };
   }
 }
 
-function getElementCaretPosition(element) {
-  return element?.selectionStart ?? 0;
-}
-
-function updateMentionBoxText(element, text, position) {
-  element.value = text;
-  element.focus();
-  element.selectionEnd = position;
-}
-
-function getElementWidthById(id) {
-  return document.getElementById(id)?.offsetWidth ?? 0;
-}
+window.mentionEditor = new Editor();
